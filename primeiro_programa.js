5e26101d8c33c8896be8e725d14fa68d9b19481d
viniciusbacaro <68190021+viniciusbacaro@users.noreply.github.com>
2021-01-22 18:03:39 -0300

    primeiro commit

---
+++ primeiro_programa.js
@@ -0,0 +1,23 @@
+// Cria as variaveis para a mensagem de boas vindas
+var greeting = 'Howdy ';
+var nome = 'Molly ';
+var = ', please check your order:';
+// Conecta as tres variaveis acima para criar a mensagem de boas vindas
+var welcome = greeting + name + message;
+
+// Cria variaveis para armazenar detalhes sobre a placa
+var sing = 'Montague House';
+var tiles = sing.length;
+var subTotal = tiles * 5;
+var shipping = 7;
+var grandTotal = subTotal + shipping
+
+
+var el = document.getElementById('greenting');
+
+
+el.textContent = welcome;
+
+
+var elSing = document.getElementById('userSing');
+elSing.textContent = sing;
